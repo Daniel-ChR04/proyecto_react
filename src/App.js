e4973fa909caf8962bbcaf2bc567ae0cf9bb407a
Daniel-ChR04 <daniel.chato@ucrso.info>
2022-07-23 18:18:46 -0600

    LoginVisual

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import List from "./components/List";
+import WithLoadingList from "./components/WithLoadingList";
 import './App.css';
+import { getFetch, postFetch, putFetch, deleteFetch } from "./components/FetchMethods";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+  return (
+    <div>
+      <h1 className="Header">Inicio de Sesión</h1>
+      <a className="Caption">Ingrese su nombre de usuario: </a>
+      <input style ={{margin: "3px"}} type="text" placeholder="Usuario" />
+      <br/>   
+      <a className="Caption">Ingrese su contraseña: </a>
+      <input style ={{margin: "3px"}} type="text" placeholder="Contraseña" />
+      <button>Aceptar</button>
+      <br/>
+      <a className="Caption">Rol: </a>
+
+
+
+
+
+    </div>
+
+  )
+}
 export default App;
